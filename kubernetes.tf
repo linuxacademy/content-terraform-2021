59f8eebf215b0891544cc43c5673b97ff025eba6
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-06-10 12:33:12 -0400

    Create kubernetes.tf

---
+++ kubernetes.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+variable "client_certificate" {
+  type = string
+}
+
+variable "client_key" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+provider "kubernetes" {
+  host = var.host
+
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+}
