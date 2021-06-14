a756e7ee50009e3e494250471e703b45d0c620c0
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-06-14 14:17:52 -0400

    Create service_eks_nginx.tf

---
+++ service_eks_nginx.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
