45c2cd4bfd307126e7f0bfb95f7f6a8e2f407e43
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-06-14 15:31:35 -0400

    Create main_w.tf

---
+++ main_w.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
