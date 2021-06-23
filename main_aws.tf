cb4c55a5486e42ae0b812bc2ed3e8712d6c9ea2c
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-06-23 16:07:06 -0400

    Update main_aws.tf
    Changes the region and the ami that was originally being used.

--- main_aws.tf
+++ main_aws.tf
@@ -11,11 +11,11 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
 
   tags = {
