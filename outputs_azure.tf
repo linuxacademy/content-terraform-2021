121ec4ed67187b12de03ac5528e2dd2986fdbb40
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-07-02 16:44:43 -0400

    Update outputs_azure.tf

--- outputs_azure.tf
+++ outputs_azure.tf
@@ -1,3 +1,7 @@
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
+output "azurerm_virtual_network_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "azurerm_subnet_name" {
+  value = azurerm_subnet.subnet.name
 }
