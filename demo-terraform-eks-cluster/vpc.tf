ce84de34739434bb2ea443de3bb500ea7eb8d52e
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-07-06 13:02:08 -0400

    Update vpc.tf

--- demo-terraform-eks-cluster/vpc.tf
+++ demo-terraform-eks-cluster/vpc.tf
@@ -1,10 +1,10 @@
 variable "region" {
-  default     = "us-east-2"
+  default     = "us-east-1"
   description = "AWS region"
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {}
