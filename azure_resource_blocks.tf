30792ae0e3eabc389ecc0a1ff806bf37dc9ca137
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-07-22 16:38:26 -0400

    Update azure_resource_blocks.tf

--- azure_resource_blocks.tf
+++ azure_resource_blocks.tf
@@ -5,13 +5,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.BatmanInc.name
   address_prefix       = "10.0.1.0/24"
 }
-
-# Create Tag
-resource "azurerm_resource_group" "rg" {
-  name     = "<Add YOUR RESOURCE GROUP NAME>"
-  location = "centralus"
-  tags = {
-    Environment = "Terraform Getting Started"
-    Team        = "Batman"
-  }
-}
