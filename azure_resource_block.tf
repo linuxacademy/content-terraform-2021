ebd7f1d49f16db21ae184a8d14ad438517e6187a
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2021-07-23 10:16:19 -0400

    Update azure_resource_block.tf

--- azure_resource_block.tf
+++ azure_resource_block.tf
@@ -3,5 +3,5 @@ resource "azurerm_subnet" "subnet" {
   name                 = "Robins"
   resource_group_name  = "<Add YOUR RESOURCE GROUP NAME>"
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.1.0/24"
+  address_prefixes       = ["10.0.1.0/24"]
 }
